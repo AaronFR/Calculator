0a38b33e11d40d210fc0db9cc1c1afd54bdef73a
AaronFR <AaronFR@users.noreply.github.com>
2019-12-16 22:31:54 +0000

    v0.5

--- Calculator.java
+++ Calculator.java
@@ -3,32 +3,55 @@ import java.util.Scanner;
  
 public class Calculator {
         
-       // public static String camelCaseToSnakeCase(String text) {
-       //        /* Simplist Solution
-       //        return input.replaceAll("(.)(\\p{Upper})", "$1_$2").toLowerCase();
+       public static double multiplyEquals(double result, double block){
+              result *= Double.valueOf(block);
+              return result;
+       }
 
-       //        (.) = 1st group matches any character
-       //        (\\p{Upper}) = pattern match to {Upper case characters}
-       //        $1, $2 = Group 1 and group 2 respectively
-       //        */
-       // }
+       public static Double operation(
+              String input
+       ){
+              String[] blocks = input.split("[-+*/^]");  // format to list of characters
+              String[] operators = input.split("[^-+*/^]+");
+
+              double result = 1;
+              int index = 0; // enumerate these
+
+              for (String block : blocks){
+                     if (index == 0){
+                            result = Double.valueOf(block);
+                     }
+                     else{
+                            String operator = operators[index];
+                            if (operator.equals("+")){
+                                   result += Double.valueOf(block); 
+                            }
+                            if (operator.equals("-")){
+                                   result -= Double.valueOf(block); 
+                            }
+                            if (operator.equals("*")){
+                                   result *= Double.valueOf(block); 
+                            }
+                            if (operator.equals("/")){
+                                   result /= Double.valueOf(block); 
+                            }
+                            if (operator.equals("^")){
+                                   result = Math.pow(result, Double.valueOf(block)); 
+                            }
+                            
+                     }
+                     index++;
+              }
+
+              return result;
+       }
     
        public static void main(String[] args) {
               Scanner in = new Scanner(System.in);
               String input = in.nextLine();
               String output = ": " + input;
 
-              if(input.contains("*")){
-                     String[] blocks = input.split("\\*");
-                     double result = 1;
-
-                     for (String block : blocks){
-                            // block = block.replaceAll("\\s","");
-                            result *= Double.valueOf(block);
-                     }
-
-                     output = String.valueOf(result);
-              }
+              output = String.valueOf(operation(input));             
               System.out.println(output);
               }
        }
